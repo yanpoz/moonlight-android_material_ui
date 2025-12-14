eb4e653fe6be05402958bdf5fc95b41de450fd5f
Yan Pazdniakou <yanpozdniakou1@gmail.com>
2025-12-14 21:36:46 +0100

    Enable edge-to-edge display in MainActivity

--- app/src/main/java/com/limelight/MainActivity.kt
+++ app/src/main/java/com/limelight/MainActivity.kt
@@ -3,6 +3,7 @@ package com.limelight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.limelight.viewmodel.MainViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
@@ -16,6 +17,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         setContent {
             MoonlightandroidTheme {
