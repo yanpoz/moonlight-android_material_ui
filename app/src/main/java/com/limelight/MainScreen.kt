2d9456ab9b17dfa2c9c0622cedc2ee7c3f70647d
yanpo <yanpozdniakou@live.com>
2026-01-23 13:19:11 +0100

    Refactor: Move AppItem to its own file

--- app/src/main/java/com/limelight/MainScreen.kt
+++ app/src/main/java/com/limelight/MainScreen.kt
@@ -1,44 +1,28 @@
 package com.limelight
 
-import android.content.Context
 import android.content.Intent
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.outlined.ExitToApp
-import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
-import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
 import androidx.compose.material.icons.outlined.AddCircleOutline
-import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material.icons.outlined.PlayArrow
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -56,7 +40,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -71,6 +54,7 @@ import com.limelight.computers.Computer
 import com.limelight.viewmodel.MainViewModel
 import com.limelight.computers.getComputerPairPinText
 import com.limelight.computers.getComputerPairStatusText
+import com.limelight.viewmodel.AppItem
 import com.limelight.viewmodel.ComputerItem
 
 
@@ -366,108 +350,6 @@ fun ConfirmationDialog(
     )
 }
 
-
-@OptIn(ExperimentalFoundationApi::class)
-@Composable
-fun AppItem(
-    viewModel: MainViewModel, app: NvApp, computer: Computer, context: Context,
-    onClick: () -> Unit, modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier
-            .aspectRatio(2f / 3f) // Vertical card (3:2 height:width)
-            .clip(CardDefaults.shape)
-            .combinedClickable(
-                onClick = onClick,
-                onLongClick = {
-                    viewModel.onAppLongClick(app.appId, computer.details.uuid)
-                }
-            )
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Bottom // Align app name to the bottom
-        ) {
-            Text( text = app.appName, style = MaterialTheme.typography.bodyLarge )
-        }
-
-        DropdownMenu(
-            modifier = Modifier.widthIn(min = 220.dp),
-            expanded = viewModel.appMenu.appId == app.appId &&
-                       viewModel.appMenu.computerUuid == computer.details.uuid,
-            onDismissRequest = { viewModel.dismissAppMenu() }
-        ) {
-            if (computer.details.runningGameId != 0) {
-                if (computer.details.runningGameId == app.appId) {
-                    // Resume Session
-                    DropdownMenuItem(
-                        text = { Text(stringResource(R.string.applist_menu_resume)) },
-                        leadingIcon = { Icon(Icons.Outlined.PlayArrow, null) },
-                        onClick = { viewModel.dismissAppMenu() /*TODO*/ }
-                    )
-                    // Quit Session
-                    DropdownMenuItem(
-                        text = { Text(stringResource(R.string.applist_menu_quit)) },
-                        leadingIcon = { Icon(Icons.Outlined.Close, null) },
-                        onClick = {
-                            viewModel.dismissAppMenu()
-                            viewModel.onQuitApp(context, app, computer.details.uuid)
-                        }
-                    )
-                    HorizontalDivider() // TODO: replace with gap Material expressive
-                }
-                else {
-                    // Quit running and Start new session
-                    DropdownMenuItem(
-                        text = { Text(stringResource(R.string.applist_menu_quit_and_start)) },
-                        leadingIcon = { Icon(Icons.AutoMirrored.Outlined.ExitToApp, null) },
-                        onClick = { viewModel.dismissAppMenu() /*TODO*/ }
-                    )
-                    HorizontalDivider() // TODO: replace with gap Material expressive
-                }
-            }
-            // Move Left TODO: should not be available when on beginning
-            DropdownMenuItem(
-                text = { Text(text = "Move Left") },
-                leadingIcon = { Icon(Icons.AutoMirrored.Outlined.KeyboardArrowLeft, null) },
-                onClick = { viewModel.dismissComputerMenu() /*TODO*/ }
-            )
-            // Move Right TODO: should not be available when on bottom
-            DropdownMenuItem(
-                text = { Text(text = "Move Right") },
-                leadingIcon = { Icon(Icons.AutoMirrored.Outlined.KeyboardArrowRight, null) },
-                onClick = { viewModel.dismissComputerMenu() /*TODO*/ }
-            )
-            HorizontalDivider() // TODO: replace with gap Material expressive
-            // App Details
-            DropdownMenuItem(
-                text = { Text(stringResource(R.string.applist_menu_details)) },
-                // leadingIcon = { Icon(Icons.AutoMirrored.Outlined.ListAlt, null) },
-                leadingIcon = { Spacer(modifier = Modifier.size(24.dp)) },
-                onClick = {
-                    viewModel.dismissAppMenu()
-                    viewModel.onAppDetailsClicked(app, computer)
-                }
-            )
-            // Create shortcut
-            DropdownMenuItem(
-                text = { Text(stringResource(R.string.applist_menu_scut)) },
-                // leadingIcon = { Icon(Icons.Outlined.StarOutline, null) },
-                leadingIcon = { Spacer(modifier = Modifier.size(24.dp)) },
-                onClick = { viewModel.dismissAppMenu() /*TODO*/ }
-            )
-            // Hide App
-            DropdownMenuItem(
-                text = { Text(stringResource(R.string.applist_menu_hide_app)) },
-                // leadingIcon = { Icon(Icons.Outlined.VisibilityOff, null) },
-                leadingIcon = { Spacer(modifier = Modifier.size(24.dp)) },
-                onClick = { viewModel.dismissAppMenu() /*TODO*/ }
-            )
-        }
-    }
-}
 
 @Preview(showBackground = true)
 @Composable
---
+++ app/src/main/java/com/limelight/viewmodel/AppItem.kt
@@ -0,0 +1,136 @@
+package com.limelight.viewmodel
+
+import android.content.Context
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ExitToApp
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.outlined.KeyboardArrowRight
+import androidx.compose.material.icons.outlined.Close
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.limelight.R
+import com.limelight.computers.Computer
+import com.limelight.nvstream.http.NvApp
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun AppItem(
+    viewModel: MainViewModel, app: NvApp, computer: Computer, context: Context,
+    onClick: () -> Unit, modifier: Modifier = Modifier.Companion
+) {
+    Card(
+        modifier = modifier
+            .aspectRatio(2f / 3f) // Vertical card (3:2 height:width)
+            .clip(CardDefaults.shape)
+            .combinedClickable(
+                onClick = onClick,
+                onLongClick = {
+                    viewModel.onAppLongClick(app.appId, computer.details.uuid)
+                }
+            )
+    ) {
+        Column(
+            modifier = Modifier.Companion
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Bottom // Align app name to the bottom
+        ) {
+            Text(text = app.appName, style = MaterialTheme.typography.bodyLarge)
+        }
+
+        DropdownMenu(
+            modifier = Modifier.Companion.widthIn(min = 220.dp),
+            expanded = viewModel.appMenu.appId == app.appId &&
+                    viewModel.appMenu.computerUuid == computer.details.uuid,
+            onDismissRequest = { viewModel.dismissAppMenu() }
+        ) {
+            if (computer.details.runningGameId != 0) {
+                if (computer.details.runningGameId == app.appId) {
+                    // Resume Session
+                    DropdownMenuItem(
+                        text = { Text(stringResource(R.string.applist_menu_resume)) },
+                        leadingIcon = { Icon(Icons.Outlined.PlayArrow, null) },
+                        onClick = { viewModel.dismissAppMenu() /*TODO*/ }
+                    )
+                    // Quit Session
+                    DropdownMenuItem(
+                        text = { Text(stringResource(R.string.applist_menu_quit)) },
+                        leadingIcon = { Icon(Icons.Outlined.Close, null) },
+                        onClick = {
+                            viewModel.dismissAppMenu()
+                            viewModel.onQuitApp(context, app, computer.details.uuid)
+                        }
+                    )
+                    HorizontalDivider() // TODO: replace with gap Material expressive
+                } else {
+                    // Quit running and Start new session
+                    DropdownMenuItem(
+                        text = { Text(stringResource(R.string.applist_menu_quit_and_start)) },
+                        leadingIcon = { Icon(Icons.AutoMirrored.Outlined.ExitToApp, null) },
+                        onClick = { viewModel.dismissAppMenu() /*TODO*/ }
+                    )
+                    HorizontalDivider() // TODO: replace with gap Material expressive
+                }
+            }
+            // Move Left TODO: should not be available when on beginning
+            DropdownMenuItem(
+                text = { Text(text = "Move Left") },
+                leadingIcon = { Icon(Icons.AutoMirrored.Outlined.KeyboardArrowLeft, null) },
+                onClick = { viewModel.dismissComputerMenu() /*TODO*/ }
+            )
+            // Move Right TODO: should not be available when on bottom
+            DropdownMenuItem(
+                text = { Text(text = "Move Right") },
+                leadingIcon = { Icon(Icons.AutoMirrored.Outlined.KeyboardArrowRight, null) },
+                onClick = { viewModel.dismissComputerMenu() /*TODO*/ }
+            )
+            HorizontalDivider() // TODO: replace with gap Material expressive
+            // App Details
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.applist_menu_details)) },
+                // leadingIcon = { Icon(Icons.AutoMirrored.Outlined.ListAlt, null) },
+                leadingIcon = { Spacer(modifier = Modifier.Companion.size(24.dp)) },
+                onClick = {
+                    viewModel.dismissAppMenu()
+                    viewModel.onAppDetailsClicked(app, computer)
+                }
+            )
+            // Create shortcut
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.applist_menu_scut)) },
+                // leadingIcon = { Icon(Icons.Outlined.StarOutline, null) },
+                leadingIcon = { Spacer(modifier = Modifier.Companion.size(24.dp)) },
+                onClick = { viewModel.dismissAppMenu() /*TODO*/ }
+            )
+            // Hide App
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.applist_menu_hide_app)) },
+                // leadingIcon = { Icon(Icons.Outlined.VisibilityOff, null) },
+                leadingIcon = { Spacer(modifier = Modifier.Companion.size(24.dp)) },
+                onClick = { viewModel.dismissAppMenu() /*TODO*/ }
+            )
+        }
+    }
+}
