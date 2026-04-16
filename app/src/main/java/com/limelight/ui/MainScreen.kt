155a170b149921f2965808c30dd4fa9f03ba5f16
valkyrie666 <ksenia.korsun@gmail.com>
2026-04-17 01:09:07 +0200

    Remove unused `Row` import in `MainScreen.kt`
    The `androidx.compose.foundation.layout.Row` import was removed from `MainScreen.kt` as it is no longer being used.

--- app/src/main/java/com/limelight/ui/MainScreen.kt
+++ app/src/main/java/com/limelight/ui/MainScreen.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.preference.PreferenceManager
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
